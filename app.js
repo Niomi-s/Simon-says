1c266a259b0680130038b067650e4712da0d7f4c
Niomi-s <niomisoni@gmail.com>
2024-05-28 16:51:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -61,7 +61,11 @@ function Check(indx){
     }
     else
     {
-        h2.innerText = `Game Over! Press any key to start.`;
+        h2.innerHTML = `Game Over! Your score was <b>${level}</b><br>Press any key to start.`;
+         document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColor = "white";
+        }, 170);
         started = false;
         gameSeq = [];
         level = 0;
